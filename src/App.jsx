a3ad6901bbd5cfa5632dab34efd05db34316fe81
saiyedkounain <syedkounainlife@gmail.com>
2026-01-27 07:42:55 +0530

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Movies from './pages/Movies'
+
+const App = () => {
+  return (
+    <div>
+      <Movies/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Movies.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Movies = () => {
+  return (
+    <div>
+        <Navbar/>
+        Movies
+    </div>
+  )
+}
+
+export default Movies
